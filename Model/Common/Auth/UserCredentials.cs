fa43673c8791daaeade255ef656fe43084ecbf8e
Alex Sebastiao Begui Pereira <alex.asbp@pmfi.pr.gov.br>
2025-04-03 09:14:41 -0300

    Ajsutado

---
+++ Model/Common/Auth/UserCredentials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CensusFieldSurvey.Model.Common.Auth
+{
+    public class UserCredentials
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+}
